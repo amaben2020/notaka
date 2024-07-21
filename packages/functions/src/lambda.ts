4668225a830331d653ef7264b23bde69534b35a0
algomachine001 <amaben202phantom@gmail.com>
2024-07-21 14:22:24 -0700

    updates

--- packages/functions/src/lambda.ts
+++
@@ -1,9 +0,0 @@
-import { ApiHandler } from 'sst/node/api';
-
-export const handler = ApiHandler(async (_evt) => {
-  console.log('_evt', _evt);
-  return {
-    statusCode: 200,
-    body: `Hello world. The time is ${new Date().toISOString()}`,
-  };
-});
